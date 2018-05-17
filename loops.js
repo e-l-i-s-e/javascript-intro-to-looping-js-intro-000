d0506f4510f41d9fd8e8bf3fd271f15de1cfe589
Elise Bonner <elise0209@mac.com>
2018-05-17 18:09:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -7,7 +7,7 @@ function forLoop(array){
     if (i === 1){
      array.push("I am 1 strange loop.");
     }
-    else if (i > 1 && i < 24) {
+    else if (i > 1 && i < 25) {
      array.push(`I am ${i} strange loops.`)
    }
   }
