d358da313175eb43bf09aebe6bbb7fcd8394266a
Elise Bonner <elise0209@mac.com>
2018-05-17 18:06:04 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,20 @@
+function forLoop(array){
+  for (var i = 0; i < 25; i++){
+  // let s = i === 1 ? "I am 1 strange loop." : `I am ${i} strange loops.`
+    if (i === 1){
+     array.push("I am 1 strange loop.");
+    }
+    else {
+     array.push(`I am ${i} strange loops.`)
+   }
+  }
+  return array;
+}
+
+function whileLoop(num) {
+  while (num > 0){
+  num--;
+  console.log(num)
+  }
+  return 'done'
+}
